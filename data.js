25cb242dc5e320557fdb7522fac53bdcdd81417c
a105ty <tanokominecraft@gmail.com>
2026-04-21 23:48:40 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+
+⇝</body>
+</html>
+
+
+    "12 resep baru menu seasonal",    "Melatih 5 barista junior"  ]}];// page 2 --------------------------------------------  -----------------const hero_label = 'Certifications && Credentials';const judul_2 = 'standar yang terfalidasi';const sec_description = 'Setiap sertifikat adalah   bukti komitmen terhadap standar tertinggi dalam   industri kopi dan F&B profesional.' ;const dataSertifikasi = [  {    categoryName: "SCA – Specialty Coffee Association"      ,    icon: "🏅",    themeColor: "#C8903C",    items: [      {        org: "SCA Authorized",        year: "2021",        name: "Barista Skills – Foundation",        grade: "Pass with Distinction",        desc: "Penguasaan dasar teknik espresso, steam           milk, dan konsistensi resep standar SCA.",certImg: "https://link-ke-gambar-sertifikat.jpg"
